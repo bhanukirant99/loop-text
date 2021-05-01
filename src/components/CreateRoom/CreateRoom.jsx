0214eac326e4646956add31e160ce6ddf167ad1f
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-01 13:09:13 +0530

    add: create room button

---
+++ src/components/CreateRoom/CreateRoom.jsx
@@ -0,0 +1,13 @@
+import { Button } from "../";
+export const CreateRoom = () => {
+  return (
+ <div className="flex justify-center z-10 ">
+
+ 
+      <Button
+        primary="btn btn-blue w-2/5 h-10 text-1xl  fixed-btn-center fixed top "
+        text="Create Room"
+      />
+    </div>
+  );
+};
