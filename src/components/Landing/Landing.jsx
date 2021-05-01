7eb1b9de0915f8474152c32f286819120a8ced1c
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-01 18:57:42 +0530

    add: landing view desktop

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,10 +3,13 @@ import './Landing.css'
 import {Button} from '../'
 export const Landing = () => {
     return (
-        <div className='landing container sm:mx-auto lg:hidden flex flex-col items-center justify-around   '>
-            <h1 className='text-5xl'>LoopText</h1>
-            <span className='text-4xl'>Never Miss the live conversation again!</span>
-            <Button primary="btn btn-blue w-2/5 h-20 text-3xl" text='Login'/>
+        <div className='landing  flex flex-col items-center justify-evenly lg:justify-center   '>
+           
+            <h1 className='text-5xl lg:text-8xl'>LoopText</h1>
+            <span className='text-4xl mt-12'>Never Miss the live conversation again!</span>
+            {/* <Button primary="btn btn-blue w-2/5 h-20 text-3xl" text='Login'/> */}
+            <img src='/images/sign_in.png' alt='sign-in' className='w-72 shadow-lg lg:w-96 rounded lg:mt-72' />
         </div>
     )
 }
+ 
