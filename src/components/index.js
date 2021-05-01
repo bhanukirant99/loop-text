75ac428d76837a0e90310dc081d06aaebee7a625
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-01 13:32:54 +0530

    rename

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
-export {Landing} from './Landing/Landing'
-export {Button} from './Button/Button'
-export {Home} from './Home/Home'
-export {Navbar} from './Navbar/Navbar'
-export {Notification} from './Notification/Notification'
-export {Search} from './Search/Search'
-export {Filter} from './Filter/Filter.jsx'
-export {RoomCard} from './RoomCard/RoomCard'
-export {RoomTabs} from './RoomTabs/RoomTabs.jsx'
-export {SearchFilter} from './SearchFilter/SearchFilter'
-export {CreateRoom} from './CreateRoom/CreateRoom'
-export {CreateRoomModal} from './Modals/CreateRoomModal'
+export { Landing } from "./Landing/Landing";
+export { Button } from "./Button/Button";
+export { Home } from "./Home/Home";
+export { Navbar } from "./Navbar/Navbar";
+export { NotificationIcon} from "./Notification/NotificationIcon";
+export { Search } from "./Search/Search";
+export { Filter } from "./Filter/Filter.jsx";
+export { RoomCard } from "./RoomCard/RoomCard";
+export { RoomTabs } from "./RoomTabs/RoomTabs.jsx";
+export { SearchFilter } from "./SearchFilter/SearchFilter";
+export { CreateRoom } from "./CreateRoom/CreateRoom";
+export { CreateRoomModal } from "./Modals/CreateRoomModal";
