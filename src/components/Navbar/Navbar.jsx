fd1493b6a9f76bd7d50e4c4c6e905e7ecae0b226
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-01 13:05:14 +0530

    add: navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './navbar.css'
+import {Brand} from './brand'
+import {Notification} from '../'
+export const Navbar = () => {
+    return (
+        <div className='navbar shadow flex flex-row justify-between items-center px-2 '>
+            <Brand/>
+            <Notification/>
+        </div>
+    )
+}
---
+++ src/components/Navbar/brand.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Brand = () => {
+    return (
+       <h1 className="text-2xl">LoopText</h1>
+    )
+}
