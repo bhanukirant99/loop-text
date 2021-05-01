bf4612deef596e7fe842933bb0695803072be5e9
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-01 13:30:12 +0530

    rename

---
+++ src/components/Notification/NotificationIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NotificationIcon = () => {
+    return (
+        <span className="material-icons-outlined">
+        notifications
+        </span>
+    )
+}
