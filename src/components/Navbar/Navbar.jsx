eb67972c4184be9c7c2dc2deb8e0797a85fda2ab
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-01 13:35:46 +0530

    add: notification icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
     return (
         <div className='navbar shadow flex flex-row justify-between items-center px-2 '>
             <Brand/>
-            <Notification/>
+            <NotificationIcon/>
         </div>
     )
 }
