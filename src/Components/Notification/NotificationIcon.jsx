66eaad8d69a856708aa88df11631f4384ecb3bf4
Maddy@Xdev200 <xdev200200@gmail.com>
2021-05-02 19:46:29 +0530

    rename: class to className

--- src/Components/Notification/NotificationIcon.jsx
+++ src/Components/Notification/NotificationIcon.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const NotificationIcon = () => {
   return (
     <div className="flex flex-row items-center">
-     <span class="material-icons-round text-4xl lg:text-5xl ">account_circle</span>
+     <span className="material-icons-round text-4xl lg:text-5xl ">account_circle</span>
     <span className="material-icons-outlined text-4xl lg:text-4xl">notifications</span>
     </div>
   );
