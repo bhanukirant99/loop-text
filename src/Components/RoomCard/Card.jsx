2546063b91c9b07c839c367cf11f932b8dcf2736
Kushank Sriraj <kushanksriraj@gmail.com>
2021-05-02 21:43:01 +0530

    feat: add to stage, audience, room

--- src/Components/RoomCard/Card.jsx
+++ src/Components/RoomCard/Card.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-export const Card = ({topic}) => {
+export const Card = ({ topic, id, callback }) => {
   return (
-    <div className="room-card flex flex-row items-center justify-center  w-96  h-52  mx-5 my-3 shadow rounded-lg relative bg-gray-100">
+    <div className="room-card flex flex-row items-center justify-center  w-96  h-52  mx-5 my-3 shadow rounded-lg relative bg-gray-100 cursor-pointer"
+    onClick={() => callback(id)}
+    >
       <div className="room-card-content ">
-        <img src='/images/live-streaming.svg' className='room-badge material-icons absolute' alt='live feed'/>
+        <img
+          src="/images/live-streaming.svg"
+          className="room-badge material-icons absolute"
+          alt="live feed"
+        />
         <h1 className="room-name text-3xl ">{topic}</h1>
       </div>
     </div>
--- src/Components/SearchFilter/SearchFilter.jsx
+++ src/Components/SearchFilter/SearchFilter.jsx
@@ -1,9 +1,9 @@
 import { Search, Filter } from "..";
-export const SearchFilter = () => {
+export const SearchFilter = ({ searchText, setSearchText, setStatus }) => {
   return (
     <div className="search-filter flex border-2 grey-red-300 lg:mx-96  justify-between m-2 my-5">
-      <Search />
-      <Filter />
+      <Search searchText={searchText} setSearchText={setSearchText} />
+      <Filter setStatus={setStatus} />
     </div>
   );
 };
