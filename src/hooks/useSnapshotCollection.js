462c3b27602408e80c114bec7724f6e68c724a20
janaki2399 <janakishreeja23@gmail.com>
2021-05-02 13:15:23 +0530

    fetch rooms

---
+++ src/hooks/useSnapshotCollection.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { useState, useEffect } from "react";
+import { useMounted } from "./useMounted";
+export const useSnapshotCollection = (query) => {
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(true);
+  const mounted = useMounted();
+
+  useEffect(() => {
+    const unsubscribe = query.onSnapshot((snapshot) => {
+      const data = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      mounted.current && setData(data);
+      setLoading && mounted.current && setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return { data, loading };
+};
